d86bad15d681afac153e9f80f4844cc1475f794b
Carlos Pena <chcp@cin.ufpe.br>
2025-07-09 16:09:19 -0300

    fix: Convert list in AnimeChunk to be str
    - required by chromadb to add as metadata

--- src/constants.py
+++ src/constants.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 CHUNKS_JSON = BASE_DIR / "data" / "index" / "chunks.json"
-CHROMA_DIR = BASE_DIR / "data" / "index" / "chroma/"
+CHROMA_DIR = BASE_DIR / "data" / "chroma/"
 RAW_DIR = BASE_DIR / "data" / "raw"
 META_DIR = BASE_DIR / "data" / "metadata"
 SUMMARY_DIR = BASE_DIR / "data" / "summaries"
